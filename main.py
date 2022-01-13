b0592012d046cf01d55fc529846bd43c755ef1ba
Sellyme <s_lamerichs@hotmail.com>
2022-01-13 10:47:59 +1030

    migrating leaderboard controller into main bot now that it's MVP

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ client = discord.Client()
 async def on_ready():
 	print(client.user, "has connected to Discord!")
 
-	await scrape_latest()
+	loop = asyncio.get_event_loop()
+	loop.create_task(scrape_latest())
+	loop.create_task(scrape_leaderboard())
 
 async def scrape_latest():
 	channel = client.get_channel(config.rss_channel)
@@ -30,7 +32,21 @@ async def scrape_latest():
 		
 		await asyncio.sleep(config.submissions_frequency)
 
-
-
+async def scrape_leaderboard():
+	print("Running leaderboard scrape")
+	channel = client.get_channel(config.test_channel)
+	while True:
+		results = scrape.scrape_leaderboard()
+		#result should be a pure string
+		print(results)
+		
+		#create embed for Discord
+		#todo - decide if we want 3 leaderboards in one embed, or 3 separate embeds w/o title
+		embed = discord.Embed(title="Leaderboards")
+		embed.add_field(name="Mainboard", value=results)
+		await channel.send(embed=embed)
+		
+		#and only run this every 24 hours
+		await asyncio.sleep(config.leaderboard_frequency)
 
 client.run(TOKEN)
