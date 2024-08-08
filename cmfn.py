9fdabf0ff2fcd55688db41bbd050ed00a7ea7622
Sellyme <s_lamerichs@hotmail.com>
2024-08-08 21:29:42 +0930

    add function to get the final leaderboard from the previous year

--- cmfn.py
+++ cmfn.py
@@ -1,4 +1,4 @@
-import math
+import math, os
 
 def time_to_seconds(time_str):
     h, m, s = time_str.split(':')
@@ -31,3 +31,23 @@ def get_flag_emoji(country_code):
 	else:
 		flag_emoji = ":flag_" + country_code + ":"
 	return flag_emoji
+
+def get_file_by_year(year, dir):
+	#takes an input year, and finds the *most recent* file in a directory matching that year
+	#this assumes all files in the directory have their yyyy-mm-dd timestamp as a filename prefix
+	year = str(year)
+	files = []
+	
+	for filename in os.listdir(dir):
+		if filename.startswith(year):
+			files.append(filename)
+	
+	#if there's no files, error out
+	if len(files) == 0:
+		print("ERROR: no files found in dir '" + dir + "' matching requested year '" + year + "'")
+		return False
+	
+	#sort all the files and take the last alphabetically (which will be the closest to the end of year)
+	req_file = sorted(files, reverse=True)[0]
+	print("DEBUG: pulling file ", req_file)
+	return req_file
